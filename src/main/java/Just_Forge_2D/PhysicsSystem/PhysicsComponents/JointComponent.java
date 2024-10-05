7d8a2b77c6edc26c6ad088c15190c4f4f83eb4f5
Asher-Ul-Haque <contactasher.u.haque@gmail.com>
2024-10-05 09:39:21 +0530

    Big Breakthrough. I have a distance joint set up

--- src/main/java/Just_Forge_2D/PhysicsSystem/PhysicsComponents/JointComponent.java
+++ src/main/java/Just_Forge_2D/PhysicsSystem/PhysicsComponents/JointComponent.java
@@ -2,8 +2,13 @@ package Just_Forge_2D.PhysicsSystem.PhysicsComponents;
 import Just_Forge_2D.EntityComponentSystem.Components.Component;
 import Just_Forge_2D.EntityComponentSystem.GameObject;
 import Just_Forge_2D.RenderingSystem.DebugPencil;
+import Just_Forge_2D.Utils.Logger;
 import Just_Forge_2D.WindowSystem.MainWindow;
 import imgui.ImGui;
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.joints.DistanceJoint;
+import org.jbox2d.dynamics.joints.DistanceJointDef;
+import org.joml.Vector2f;
 
 
 public class JointComponent extends Component
@@ -11,6 +16,10 @@ public class JointComponent extends Component
     private String otherName;
     private transient GameObject other;
     private transient RigidBodyComponent otherRB;
+    private transient DistanceJointDef defJoint = new DistanceJointDef ();
+    private transient DistanceJoint joint;
+    private transient boolean firstTime = false;
+
 
     public JointComponent(GameObject OTHER)
     {
@@ -31,34 +40,59 @@ public class JointComponent extends Component
 
     public JointComponent() {}
 
-    @Override
-    public void start()
+    private void createJoint()
     {
         if (other == null)
         {
             other = MainWindow.getCurrentScene().getGameObject(otherName);
+            otherRB = other.getComponent(RigidBodyComponent.class);
+        }
+        if (other != null && otherRB != null)
+        {
+            defJoint.initialize(this.gameObject.getComponent(RigidBodyComponent.class).getRawBody(), otherRB.getRawBody(), new Vec2(this.gameObject.transform.position.x, this.gameObject.transform.position.y), new Vec2(other.transform.position.x, other.transform.position.y));
+            joint = (DistanceJoint) MainWindow.getCurrentScene().getPhysics().rawWorld.getWorld().createJoint(defJoint);
         }
     }
 
     @Override
     public void editorUpdate(float DELTA_TIME)
     {
-        if (other != null) DebugPencil.addLine(other.transform.position, this.gameObject.transform.position);
+        if (joint != null)
+        {
+            Vec2 pointA = new Vec2();
+            Vec2 pointB = new Vec2();
+            joint.getAnchorA(pointA);
+            joint.getAnchorB(pointB);
+            DebugPencil.addLine(new Vector2f(pointA.x, pointA.y), new Vector2f(pointB.x, pointB.y));
+        }
     }
 
     @Override
     public void update(float DELTA_TIME)
     {
+        if (!firstTime)
+        {
+            try
+            {
+                createJoint();
+            }
+            catch (Exception e)
+            {
+                Logger.FORGE_LOG_ERROR("sry");
+            }
+            firstTime = true;
+        }
         editorUpdate(DELTA_TIME);
     }
+
 
     @Override
     public void editorGUI()
     {
         super.editorGUI();
-        if (ImGui.button("Set other"))
+        if (ImGui.button("Set other | Joint : " + (joint == null)))
         {
-            other = MainWindow.getCurrentScene().getGameObject(otherName);
+            createJoint();
         }
     }
 }
