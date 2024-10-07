2cc165eafcd408cb8fc020968514488eb736ad5c
Asher-Ul-Haque <contactasher.u.haque@gmail.com>
2024-10-07 11:19:38 +0530

    Bad Luck with Hinge Joint, study physics a bit more

--- src/main/java/Just_Forge_2D/PhysicsSystem/PhysicsComponents/Joints/HingeComponent.java
+++ src/main/java/Just_Forge_2D/PhysicsSystem/PhysicsComponents/Joints/HingeComponent.java
@@ -1,9 +1,11 @@
 package Just_Forge_2D.PhysicsSystem.PhysicsComponents.Joints;
 
+import Just_Forge_2D.EditorSystem.Widgets;
 import Just_Forge_2D.EntityComponentSystem.GameObject;
 import Just_Forge_2D.PhysicsSystem.PhysicsComponents.RigidBodyComponent;
 import Just_Forge_2D.RenderingSystem.DebugPencil;
 import Just_Forge_2D.WindowSystem.MainWindow;
+import imgui.ImGui;
 import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.joints.RevoluteJoint;
 import org.jbox2d.dynamics.joints.RevoluteJointDef;
@@ -22,8 +24,7 @@ public class HingeComponent extends BaseJointComponent
     protected boolean collideConntected = false;
     protected float referenceAngle = 0f;
     protected boolean enableLimit = false;
-    protected float upperAngle = (float) (Math.PI / 2f);
-    protected float lowerAngle = -(float) (Math.PI / 2f);;
+    protected Vector2f limits = new Vector2f((float) (Math.PI / 2f), -(float) (Math.PI / 2f));
     protected boolean enableMotor = true;
     protected float motorSpeed = 5f;
     protected float maxMotorTorque = 20f;
@@ -31,6 +32,153 @@ public class HingeComponent extends BaseJointComponent
 
     // - - - | Functions | - - -
 
+
+    // - - - | Getters And Setters | - - -
+
+
+    // - - - anchors - - -
+
+    // - - - anchorA
+    public void setAnchorA(Vector2f ANCHOR)
+    {
+        this.anchorA = ANCHOR;
+        joint = null;
+        createJoint();
+    }
+
+    public Vector2f getAnchorA()
+    {
+        Vec2 temp = new Vec2();
+        if (this.joint != null)
+        {
+            joint.getAnchorA(temp);
+            this.anchorA.x = temp.x;
+            this.anchorA.y = temp.y;
+        }
+        return this.anchorA;
+    }
+
+    // - - - anchor B
+    public void setAnchorB(Vector2f ANCHOR)
+    {
+        this.anchorB = ANCHOR;
+        joint = null;
+        createJoint();
+    }
+
+    public Vector2f getAnchorB()
+    {
+        Vec2 temp = new Vec2();
+        if (this.joint != null)
+        {
+            joint.getAnchorB(temp);
+            this.anchorB.x = temp.x;
+            this.anchorB.y = temp.y;
+        }
+        return this.anchorB;
+    }
+
+    // - - - hinge anchor
+    public void setHingeAnchor(Vector2f ANCHOR)
+    {
+        hingeAnchor = ANCHOR;
+        joint = null;
+        createJoint();
+    }
+
+    public Vector2f getHingeAnchor()
+    {
+        return hingeAnchor;
+    }
+
+
+    // - - - Collide
+    public boolean isCollideConnected()
+    {
+        if (this.joint != null) this.collideConntected = joint.getCollideConnected();
+        return this.collideConntected;
+    }
+
+    public void collideConnect(boolean REALLY)
+    {
+        this.collideConntected = REALLY;
+    }
+
+
+    // - - - Angles - - -
+
+    // - - - reference
+    public float getReferenceAngle() { return this.referenceAngle; }
+
+    public void setReferenceAngle(float ANGLE)
+    {
+        this.referenceAngle = ANGLE;
+        this.joint = null;
+        createJoint();
+    }
+
+    // - - - limit enable
+    public boolean isLimitEnabled()
+    {
+        return enableLimit;
+    }
+
+    public void enableLimit(boolean REALLY)
+    {
+        this.enableLimit = REALLY;
+        if (this.joint != null) this.joint.enableLimit(REALLY);
+    }
+
+    // - - - limits
+    public void setLimits(Vector2f LIMITS)
+    {
+        this.limits = LIMITS;
+        if (this.joint != null) this.joint.setLimits(LIMITS.x, LIMITS.y);
+    }
+
+    public Vector2f getLimits()
+    {
+        return this.limits;
+    }
+
+
+    // - - - Motor - - -
+
+    // - - - enable
+    public boolean isMotorEnabled()
+    {
+        return this.enableMotor;
+    }
+
+    public void enableMotor(boolean REALLY)
+    {
+        this.enableMotor = REALLY;
+        if (this.joint != null) this.joint.enableMotor(REALLY);
+    }
+
+    // - - - speed
+    public float getMotorSpeed()
+    {
+        return this.motorSpeed;
+    }
+
+    public void setMotorSpeed(float SPEED)
+    {
+        this.motorSpeed = SPEED;
+        if (this.joint != null) this.joint.setMotorSpeed(SPEED);
+    }
+
+    // - - - torque
+    public float getMaxMotorTorque()
+    {
+        return this.maxMotorTorque;
+    }
+
+    public void setMaxMotorTorque(float TORQUE)
+    {
+        this.maxMotorTorque = TORQUE;
+        if (this.joint != null) this.joint.setMaxMotorTorque(TORQUE);
+    }
 
     // - - - constructors - - -
 
@@ -58,8 +206,8 @@ public class HingeComponent extends BaseJointComponent
             defJoint.enableLimit = this.enableLimit;
             defJoint.enableMotor = this.enableMotor;
             defJoint.collideConnected = this.collideConntected;
-            defJoint.lowerAngle = this.lowerAngle;
-            defJoint.upperAngle = this.upperAngle;
+            defJoint.upperAngle = this.limits.x;
+            defJoint.lowerAngle = this.limits.y;
             defJoint.maxMotorTorque = this.maxMotorTorque;
             defJoint.motorSpeed = this.motorSpeed;
             // - - - TODO: abstract stuff out check for null instead of 0.
@@ -74,7 +222,63 @@ public class HingeComponent extends BaseJointComponent
     @Override
     public void debugDraw()
     {
-        DebugPencil.addCircle(hingeAnchor, 0.5f);
-        DebugPencil.addLine(anchorA, anchorB);
+        DebugPencil.addCircle(getHingeAnchor(), 0.5f, color);
+        DebugPencil.addLine(getHingeAnchor(), this.gameObject.transform.position);
+        if (other != null) DebugPencil.addLine(getHingeAnchor(), other.transform.position);
+    }
+
+    @Override
+    public void editorGUI()
+    {
+        super.editorGUI();
+
+        Widgets.colorPicker3("Joint Color", color);
+
+        Vector2f temp = new Vector2f(getHingeAnchor());
+        Widgets.drawVec2Control("Hinge Anchor", temp);
+        if (!temp.equals(getHingeAnchor())) setHingeAnchor(temp);
+        if (ImGui.button("Reset Hinge Anchor"))
+        {
+            this.hingeAnchor = new Vector2f();
+            this.joint = null;
+            createJoint();
+        }
+
+        temp = new Vector2f(getLimits());
+        Widgets.drawVec2Control("Angle Limits", temp);
+        if (!temp.equals(getLimits())) setLimits(temp);
+
+        temp = new Vector2f(getAnchorA());
+        Widgets.drawVec2Control("Anchor A", temp);
+        if (!temp.equals(getAnchorA())) setAnchorA(temp);
+        if (ImGui.button("Reset Anchor A"))
+        {
+            this.anchorA = new Vector2f();
+            this.joint = null;
+            createJoint();
+        }
+
+        temp = new Vector2f(getAnchorB());
+        Widgets.drawVec2Control("Anchor B", temp);
+        if (!temp.equals(getAnchorB())) setAnchorB(temp);
+        if (ImGui.button("Reset Anchor B"))
+        {
+            this.anchorB = new Vector2f();
+            this.joint = null;
+            createJoint();
+        }
+
+        collideConnect(Widgets.drawBoolControl("Collide with Connection", isCollideConnected()));
+
+        float tempF = Widgets.drawFloatControl("Reference Angle", getReferenceAngle());
+        if (tempF != getReferenceAngle()) setReferenceAngle(tempF);
+
+        enableLimit(Widgets.drawBoolControl("Enable Limits", isLimitEnabled()));
+
+        enableMotor(Widgets.drawBoolControl("Enable Motor", isMotorEnabled()));
+
+        setMaxMotorTorque(Widgets.drawFloatControl("Max Torque", getMaxMotorTorque()));
+
+        setMotorSpeed(Widgets.drawFloatControl("Motor Speed", getMotorSpeed()));
     }
 }
